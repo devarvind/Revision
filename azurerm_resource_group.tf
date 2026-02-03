c36a5614137b3f7724340d58bb2f88a7a8461a90
devarvind <arvinddevops17@gmail.com>
2026-02-03 13:30:12 +0530

    one resource group added

--- azurerm_resource_group.tf
+++ azurerm_resource_group.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "test" {
   name     = "03feb-rg"
   location = "centralindia"
 }
+
+resource "azurerm_resource_group" "test" {
+  name     = "03feb-rg1"
+  location = "centralindia"
+}
